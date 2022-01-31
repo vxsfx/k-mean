fd7f336e517d96d83b5aeca774140abff10612ac
ESCG Computing <513035@chrome.escg.ac.uk>
2022-01-31 13:43:57 +0000

    fixed kmeans

--- kmean.py
+++ kmean.py
@@ -33,12 +33,12 @@ def graph(data, k_points):
     #plt.show()
 
 def get_V(k_points, d_point):
-    short_v = [d_point[x] - k_points[0][x] , d_point[y] - k_points[0][y]]
+    short_v = [k_points[0][x] -d_point[x] ,  k_points[0][y] - d_point[y]]
     short_d = math.sqrt(short_v[x]*short_v[x] + short_v[y]*short_v[y] )
     k = 0
     
-    for kp_ind in range(0, k_points.__len__()):
-        v = [d_point[x] - k_points[kp_ind][x] , d_point[y] - k_points[kp_ind][y]]
+    for kp_ind in range(1, k_points.__len__()):
+        v = [k_points[kp_ind][x] - d_point[x], k_points[kp_ind][y]- d_point[y]]
         dist = math.sqrt(v[x]*v[x] + v[y]*v[y] )
         if dist < short_d:
             short_d = dist
@@ -57,12 +57,26 @@ def k_mean(data, iterations, k_val):
     k_means = [[0,0] for i in range(k_val)]
 
     for i in range(iterations):
+        #k_means = [[0,0] for i in range(k_val)]
+        # use totals instead of vect / 2 => for each point ???
+        k_totals = [0 for i in range(k_val)]
         for vector in data:
             t_v, k_index  = get_V(k_points, vector)
-            k_means[k_index][x] = (k_means[k_index][x] + t_v[x]) / 2
-            k_means[k_index][y] = (k_means[k_index][y] + t_v[y] ) / 2   #ffffffffffffffuuuuuuuuuuuuu y was x :(
+            k_totals[k_index] += 1
+            k_means[k_index][x] -= t_v[x]#(k_means[k_index][x] - t_v[x]) /2 
+            k_means[k_index][y] -= t_v[y] #(k_means[k_index][y] - t_v[y] ) /2   #ffffffffffffffuuuuuuuuuuuuu y was x :(
+            
+            
+            
+            #k_means[k_index]
+        for k_ind in range(k_val):
+            k_means[k_ind][x] = k_means[k_ind][x] / k_totals[k_ind]
+            k_means[k_ind][y] = k_means[k_ind][y] / k_totals[k_ind]    #ffffffffffffffuuuuuuuuuuuuu y was x :(
+            
+
+
         for kp in range(k_val):
-             k_points[kp] = [k_means[kp][x] + k_points[kp][x], k_means[kp][y] + k_points[kp][y]]
+             k_points[kp] = [k_points[kp][x] + k_means[kp][x], k_points[kp][y] + k_means[kp][y]]
             #k_points[kp] = k_means[kp].copy()
 
         
@@ -78,8 +92,8 @@ if __name__ == "__main__":
     d_bl = [[x, y] for x,y in zip(np.random.randint(0, 30, 50), np.random.randint(0, 15, 50))]
     d_tr = [[x, y] for x,y in zip(np.random.randint(40, 50, 50), np.random.randint(40, 50, 50))]
 
-    d_tl = [[x, y] for x,y in zip(np.random.randint(0, 20, 50), np.random.randint(40, 50, 50))]
-    d_br = [[x, y] for x,y in zip(np.random.randint(30, 50, 50), np.random.randint(0, 10, 50))]
+    d_tl = [[x, y] for x,y in zip(np.random.randint(10, 40, 50), np.random.randint(40, 50, 50))]
+    d_br = [[x, y] for x,y in zip(np.random.randint(40, 50, 50), np.random.randint(0, 10, 50))]
 
     data = d_bl + d_tr + d_tl + d_br
 
